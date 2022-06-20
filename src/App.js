10f6bec7202cc3412064dfe14e248351ffb607a5
ssucode <ssuya79@gmail.com>
2022-06-20 14:56:01 +0900

    feat:TodoList Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
+import Input from './components/input';
+import Todo from './components/todo';
 
 function App() {
+  const baseUrl = "http://localhost:8081"
+
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+
+  useEffect(() => {
+    getTodos()
+  },[])
+
+  async function getTodos() {
+    await axios
+    .get(baseUrl + "/todo")
+    .then((response) => {
+      setTodos(response.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  function insertTodo(e) {
+    e.preventDefault()
+
+    const insertTodo = async () => {
+      await axios
+      .post(baseUrl + "/todo", {
+        todoName: input
+      })
+      .then((response) => {
+        console.timeLog(response.data)
+        setInput("")
+        getTodos()
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    }
+
+    insertTodo()
+    console.log("add todo")
+  }
+
+  function updateTodo(id) {
+
+    const updateTodo = async () => {
+      await axios
+      .put(baseUrl + "/todo/" + id, {})
+      .then((response) => {
+        setTodos(
+          todos.map((todo) => 
+            todo.id === id ? { ...todo, completed: !todo.completed} : todo
+          )
+        )
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    }
+
+    updateTodo()
+    console.log("update todo")
+  }
+
+  function deleteTodo(id) {
+
+    const deleteTodo = async () => {
+      await axios
+      .delete(baseUrl + "/todo/" + id, {})
+      .then((response) => {
+        setTodos(
+          todos.filter((todo) => todo.id !== id) 
+        )
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    }
+    
+    deleteTodo()
+    console.log("delete todo")
+      
+  }
+
+  function changeText(e) {
+    e.preventDefault()
+    setInput(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST</h1>
+      <Input handleSubmit={insertTodo} input={input} handleChange={changeText}/>
+
+      {
+        todos
+        ? 
+        todos.map((todo) => {
+          return (
+            <Todo key={todo.id} todo={todo} handleClick={() => updateTodo(todo.id)} handleDelete={() => deleteTodo(todo.id)}/>
+          )
+        })
+        : null
+      }
     </div>
   );
 }
